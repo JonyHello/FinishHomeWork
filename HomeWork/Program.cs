92c3f53fd00eb871d7f685aa122e691719efaecf
Jony <trallipop43@gmail.com>
2023-03-13 19:58:21 +0300

    FinishHomeWork_1.0

---
+++ HomeWork/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.Write("Введите длину массива: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+string[] str = CreateArrayString(arrayLength);
+System.Console.Write("Исходный массив: ");
+PrintArray(str);
+System.Console.WriteLine();
+string[] newStr = NewArray(str);
+System.Console.Write("Новый массив: ");
+PrintArray(newStr);
+Console.ReadKey();
+
+
+
+void PrintArray(string[] array)
+{
+    for(int i = 0;i<array.Length;i++)
+    {
+        System.Console.Write($"\"{array[i]}\" ");
+    }
+    
+}
+
+string[] NewArray(string[] array)
+{
+    int arrLength = 0;
+    for(int i  = 0;i<array.Length;i++)
+    {
+        int k = 0;
+        foreach(char s in array[i])
+            k++;
+        if(k<=3)
+            arrLength++;
+    }
+    string[] newArray = new string[arrLength];
+    int j = 0;
+    for(int i = 0;i<array.Length;i++)
+    {
+        int k = 0;
+        foreach(char c in array[i])
+            k++;
+        if(k<=3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+string[] CreateArrayString(int x)
+{
+    string[] array = new string[x];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    Console.Clear();
+    return array;
+}
